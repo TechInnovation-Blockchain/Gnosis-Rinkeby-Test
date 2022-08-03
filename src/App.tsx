585e2912597265af6c5998857624f71375982eb1
TechInnovation-Blockchain <newcaesar628@gmail.com>
2022-08-03 15:03:45 -0500

    example for rinkey eth

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react'
+import styled from 'styled-components'
+import { Button, TextField, Title } from '@gnosis.pm/safe-react-components'
+import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk'
+import BalancesTable from './components/BalancesTable'
+import { useSafeBalances } from './hooks/useSafeBalances'
+import { getTransferTransaction } from './api/transfers'
+
+const Container = styled.div`
+  padding: 1rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const SafeApp = (): React.ReactElement => {
+  const { sdk, safe } = useSafeAppsSDK();
+  const [balances] = useSafeBalances(sdk);
+  console.log(balances)
+  const [recipient, setRecipient] = React.useState('');
+
+  const handleTransfer = async (): Promise<void> => {
+    const transactions = balances.map((balance) => getTransferTransaction(balance, recipient));
+  
+    const { safeTxHash } = await sdk.txs.send({ txs: transactions });
+  
+    console.log({ safeTxHash });
+  };
+
+  return (
+    <Container>
+      <Title size="sm">Safe: {safe.safeAddress}</Title>
+      <BalancesTable balances={balances} />
+
+      <TextField
+        label="Recipient"
+        onChange={(e) => {
+          setRecipient(e.target.value);
+        }}
+        value={recipient}
+      />
+      <Button size="lg" color="primary" onClick={handleTransfer}>
+        Send the assets
+      </Button>
+    </Container>
+  );
+};
+
+export default SafeApp;
